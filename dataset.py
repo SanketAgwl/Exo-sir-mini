c62d808550d50c3f104598e41ae2f9d98582f3a6
SanketAgwl <79307415+SanketAgwl@users.noreply.github.com>
2022-12-09 12:15:37 +0530

    dataset updated

--- dataset.py
+++ dataset.py
@@ -21,37 +21,22 @@ except:
     print("Errorrrrrrrrrrrr")
 usernameList=[]
 for q in query:
-    tweets = tweepy.Cursor(api.search_tweets,q, lang="en").items(10)
+    tweets = tweepy.Cursor(api.search_tweets,q, lang="en").items(50)
     list_tweets = [tweet for tweet in tweets]
     for tweet in list_tweets:
         usernameList.append(tweet.user.screen_name)
+
+usernameList = [*set(usernameList)]
+
 print(usernameList)
 print(len(usernameList))
 
-def printtweetdata(n, ith_tweet):
-    print()
-    print(f"Tweet {n}:")
-    print(f"Username:{ith_tweet[0]}")
-    print(f"Description:{ith_tweet[1]}")
-    print(f"Location:{ith_tweet[2]}")
-    print(f"Following Count:{ith_tweet[3]}")
-    print(f"Follower Count:{ith_tweet[4]}")
-    print(f"Total Tweets:{ith_tweet[5]}")
-    print(f"Retweet Count:{ith_tweet[6]}")
-    print(f"Tweet Text:{ith_tweet[7]}")
-    print(f"Hashtags Used:{ith_tweet[8]}")
-
-
 
 db = pd.DataFrame(columns=['username',
-                           'description',
-                           'location',
-                           'following',
-                           'followers',
-                           'totaltweets',
-                           'retweetcount',
-                           'text',
-                           'hashtags'])
+                           'hashtag',
+                           'first tweet',
+                           'last tweet',
+                           'difference'])
 
 
 
@@ -59,49 +44,48 @@ for i in usernameList:
     userID =i
     tweets = api.user_timeline(screen_name=userID,
                                # 200 is the maximum allowed count
-                               count=10,
+                               count=50,
                                include_rts=False, )
     list_tweets = [tweet for tweet in tweets]
 
     # Counter to maintain Tweet Count
     i = 1
-
+    ret= {}
     # we will iterate over each tweet in the
     # list for extracting information about each tweet
-    for tweet in list_tweets:
-        username = tweet.user.screen_name
-        description = tweet.user.description
-        location = tweet.user.location
-        following = tweet.user.friends_count
-        followers = tweet.user.followers_count
-        totaltweets = tweet.user.statuses_count
-        retweetcount = tweet.retweet_count
-        hashtags = tweet.entities['hashtags']
-        # Retweets can be distinguished by
-        # a retweeted_status attribute,
-        # in case it is an invalid reference,
-        # except block will be executed
-        try:
-            text = tweet.retweeted_status.text
-        except AttributeError:
-            text = tweet.text
-        hashtext = list()
-        for j in range(0, len(hashtags)):
-            hashtext.append(hashtags[j]['text'])
 
-        # Here we are appending all the
-        # extracted information in the DataFrame
-        ith_tweet = [ username, description,
-                     location, following,
-                     followers, totaltweets,
-                     retweetcount,text, hashtext]
-        db.loc[len(db)] = ith_tweet
+    for tweet in list_tweets:
+        # print(tweet.entities['hashtags'])
+        for l in tweet.entities['hashtags']:
+            word = l['text']
+            if word in ret:
+                ret[word] += [tweet]
+            else:
+                ret[word] = []
+            # print(word)
+
+    if len(ret) == 0:
+        continue
+
+    sum = 0
+
+    for hashtweets in ret.items():
+        c = hashtweets[1]
+        if len(c) > 0:
+            a = hashtweets[1][0]
+            b = hashtweets[1][-1]
+            # print(a.created_at)
+            diff = a.created_at - b.created_at
+            hashtext = hashtweets[0]
+
+            # Here we are appending all the
+            # extracted information in the DataFrame
+            ith_tweet = [ userID, hashtext, a.created_at, b.created_at, diff]
+            print(ith_tweet)
+            db.loc[len(db)] = ith_tweet
 
         # Function call to print tweet data on screen
-        printtweetdata(i, ith_tweet)
-        i = i + 1
     filename = 'scraped_tweets.csv'
-
+    df= db.drop_duplicates
     # we will save our database as a CSV file.
     db.to_csv(filename)
-
