5869f691fd2f25bdeb836e4b8a8c4193f3ddd913
Utkarsh Rai <utkarshr348@gmail.com>
2022-12-09 16:09:29 +0530

    finally done

--- calculate.py
+++ calculate.py
@@ -1,24 +1,28 @@
 import pandas as pd
 from datetime import datetime as dt , timedelta as td
 from datetime import time as t
-avg_time = td(days=0,hours=12, minutes=0,seconds=0) # average time
-print(avg_time)
+# avg_time = td(days=0, hours=12, minutes=0,seconds=0) # average time
+# print(avg_time)
 filename = pd.read_csv('scraped_tweets.csv')
-df=pd.DataFrame(filename, columns=['difference'])
-squared_error = []
-l=len(df.index)
+df = pd.DataFrame(filename, columns=['difference'])
+diff_in_sec = []
 for i in df.index:
    # print(df.loc[i].difference)
    #print(type(df.loc[i].difference))
    #print(type(pd.to_timedelta(df.loc[i].difference)))
-   temp=pd.to_timedelta(df.loc[i].difference)-avg_time
+   temp=pd.to_timedelta(df.loc[i].difference)
    temp2=temp.total_seconds();
-   squared_error.append(temp2*temp2);
+   diff_in_sec.append(temp2);
    #squared_error.append(pd.to_timedelta(df.loc[i].difference)-avg_time).total_seconds()*(pd.to_timedelta(df.loc[i].difference)-avg_time).total_seconds()
-filename['Squared_error']=squared_error
-print(squared_error)
+filename['Diff in seconds']=diff_in_sec
+print(diff_in_sec)
 
 filename.to_csv('scraped_tweets.csv',index=False)
 # print(df)
 # print(len(df.index))
+
+#calculate standard mean from error
+df2=pd.DataFrame(filename,columns=['Diff in seconds'])
+standard_mean_error=df2.sem(axis=0)
+print(standard_mean_error)
 
